3b25d42efe6fa8d36230c462ac236fc2b5f8e1a0
csxiaodiao <i@xiaodiao.me>
2016-08-26 15:37:35 +0800

    init

---
+++ config/deploy.rb
@@ -0,0 +1,39 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
+set :application, 'shengwei_php'
+set :repo_url, "git@github.com:csxiaodiao/xiaqi.git"
+set :repo_tree, 'website'
+
+ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+
+set :deploy_to, "/var/apps/#{fetch(:application)}_#{fetch(:stage)}"
+
+set :linked_files, %w{
+  caches/configs/database.php
+  phpsso_server/caches/configs/database.php
+}
+
+set :linked_dirs, %w{
+  uploadfile
+}
+
+set :keep_releases, 20
+
+set :ssh_options, {forward_agent: true}
+
+# rvm
+set :rvm_ruby_version, "2.2.2@#{fetch(:application)}_#{fetch(:stage)}"
+
+namespace :deploy do
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+        # execute "rm -rf #{release_path}/data/cache/*"
+      # end
+    end
+  end
+
+end
